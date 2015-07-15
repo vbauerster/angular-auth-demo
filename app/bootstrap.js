7aad3372ec9a922928abd22e25c33102cde7a934
Vladimir Bauer <vbauerster@gmail.com>
2015-07-15 11:22:41 +0300

    angular root is document.body

--- app/bootstrap.js
+++ app/bootstrap.js
@@ -2,5 +2,5 @@ import angular from 'angular';
 import main from 'src/app';
 
 angular.element(document).ready(function() {
-    angular.bootstrap(document, [main.name]);
+    angular.bootstrap(document.body, [main.name]);
 });
